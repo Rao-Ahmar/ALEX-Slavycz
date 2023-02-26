2c6f2a3246bf852df48aa8f964e0e67fb3ef0008
Rao-Ahmar <raoahmar87@gmail.com>
2023-02-26 13:57:30 +0500

    Adjusting viewport iphone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,17 @@ import "../styles/globals.css";
 import "../styles/fonts.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
